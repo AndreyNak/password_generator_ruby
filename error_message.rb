a2b54e3d340247fbb38a8ebb305a3da2da8f34ad
canina1337 <46906916+canina1337@users.noreply.github.com>
2021-04-21 20:47:42 +0300

    Update error_message.rb

--- error_message.rb
+++ error_message.rb
@@ -1,19 +1,19 @@
 module ErrorMessage
 
   def should_integer_letters_message
-    puts('В Поле "Длина букв" должна быть цифрой, типом integer')
+    puts('В Поле "Длина букв" значение  должно быть цифрой, типом integer')
   end
 
   def should_integer_numbers_message
-    puts('В Поле "Длина чисел" должна быть цифрой, типом integer')
+    puts('В Поле "Длина чисел" значение  должно быть цифрой, типом integer')
   end
 
   def max_value_letters_message
-    puts('"Длина числа"  должна быть цифрой, типом integer')
+    puts('В поле "Длина числа" значение должно быть не больше 50')
   end
 
   def max_value_numbers_message
-    puts('Длина числа должна быть не больше 30 значений')
+    puts('В поле "Длина числа" значение должно быть не больше 30')
   end
 
   def without_zero_message
@@ -21,15 +21,15 @@ module ErrorMessage
   end
 
   def set_numbers_should_boolean_message
-    puts('Поле  "Добавить числа" должен быть только  типом boolean')
+    puts('Поле  "Добавить числа" значение должно быть только  типом boolean')
   end
 
   def right_numbers_should_boolean_message
-    puts('Поле  "Правые числа" должен быть только типом boolean')
+    puts('Поле  "Правые числа" значение должно быть только типом boolean')
   end
 
   def left_numbers_should_boolean_message
-    puts('Поле  "Левые числа" должен быть только  типом boolean')
+    puts('Поле  "Левые числа" значение должно только  типом boolean')
   end
 
   def should_integer_global_message
