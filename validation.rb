9d95ae136e3a48efff173b118c25ffce6d7eaa85
canina1337 <46906916+canina1337@users.noreply.github.com>
2021-04-22 21:21:20 +0300

    Update validation.rb

--- validation.rb
+++ validation.rb
@@ -3,24 +3,27 @@ module Validation
   include ErrorMessage
 
   def validation_values
-    count_true = 0
     begin
-      @lengthLetters == @lengthLetters.to_i ? count_true += 1 : should_integer_letters_message
-      @lengthLetters <= 50 ? count_true += 1 : max_value_letters_message
-      @lengthLetters >= 0 ? count_true += 1 : max_value_letters_message
-      @lengthNumbers == @lengthNumbers.to_i ? count_true += 1 : should_integer_numbers_message
-      @lengthNumbers >= 1 ? count_true += 1 : min_value_numbers_message
-      @lengthNumbers <= 30 ? count_true += 1 : max_value_numbers_message
-      !!@setNumbers == @setNumbers ? count_true += 1 : set_numbers_should_boolean_message
-      !!@leftNumbers == @leftNumbers ? count_true += 1 : left_numbers_should_boolean_message
-      !!@rightNumbers == @rightNumbers ? count_true += 1 : right_numbers_should_boolean_message
+      valid_mass = [
+        @lengthLetters == @lengthLetters.to_i ? true : should_integer_letters_message,
+        @lengthLetters <= 50 ? true : max_value_letters_message,
+        @lengthLetters >= 0 ? true : max_value_letters_message,
+        @lengthNumbers == @lengthNumbers.to_i ? true : should_integer_numbers_message,
+        @lengthNumbers >= 1 ? true : min_value_numbers_message,
+        @lengthNumbers <= 30 ? true : max_value_numbers_message,
+        !!@setNumbers == @setNumbers ? true : set_numbers_should_boolean_message,
+        !!@leftNumbers == @leftNumbers ? true : left_numbers_should_boolean_message,
+        !!@rightNumbers == @rightNumbers ? true : right_numbers_should_boolean_message
+      ]
 
     rescue ArgumentError
       return should_integer_global_message
     rescue NoMethodError
       return should_integer_global_message
     end
-    puts(count_true)
-    true if count_true === 9
+    return false if valid_mass.include?(false)
+
+    true
+
   end
 end
