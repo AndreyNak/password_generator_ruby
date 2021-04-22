b468a7c41b82692a3fa964d992e3dac4e0947d0e
canina1337 <46906916+canina1337@users.noreply.github.com>
2021-04-22 21:28:38 +0300

    Update error_message.rb

--- error_message.rb
+++ error_message.rb
@@ -1,26 +1,32 @@
 module ErrorMessage
   def should_integer_letters_message
     puts('В Поле "Длина букв" значение  должно быть цифрой, типом integer')
+    false
   end
 
   def should_integer_numbers_message
     puts('В Поле "Длина чисел" значение  должно быть цифрой, типом integer')
+    false
   end
 
   def max_value_letters_message
     puts('В поле "Длина числа" значение должно быть не больше 50')
+    false
   end
 
   def min_value_letters_messages
     puts('В поле "Длина числа" значение должно быть не меньше 0')
+    false
   end
 
   def max_value_numbers_message
     puts('В поле "Длина числа" значение должно быть не больше 30')
+    false
   end
 
   def min_value_numbers_message
     puts('В поле "Длина числа" значение должно быть не меньше 1')
+    false
   end
 
   def without_zero_message
@@ -29,17 +35,24 @@ module ErrorMessage
 
   def set_numbers_should_boolean_message
     puts('Поле  "Добавить числа" значение должно быть только  типом boolean')
+    false
   end
 
   def right_numbers_should_boolean_message
     puts('Поле  "Правые числа" значение должно быть только типом boolean')
+    false
   end
 
   def left_numbers_should_boolean_message
     puts('Поле  "Левые числа" значение должно только  типом boolean')
+    false
   end
 
   def should_integer_global_message
     puts('Введите в поле  значения типа integer !')
+    false
   end
+
+
+
 end
