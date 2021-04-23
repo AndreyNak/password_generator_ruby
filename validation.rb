71d9b6a178d6ff320b65f357ea8306837197d876
canina1337 <46906916+canina1337@users.noreply.github.com>
2021-04-24 00:39:42 +0300

    Update validation.rb

--- validation.rb
+++ validation.rb
@@ -1,19 +1,23 @@
+# frozen_string_literal: true
+
 require_relative 'error_message'
+
+# Some documentation about Validation
 module Validation
   include ErrorMessage
 
   def validation_values
     begin
       valid_mass = [
-        @lengthLetters == @lengthLetters.to_i ? true : should_integer_letters_message,
-        @lengthLetters <= 50 ? true : max_value_letters_message,
-        @lengthLetters >= 0 ? true : min_value_letters_messages,
-        @lengthNumbers == @lengthNumbers.to_i ? true : should_integer_numbers_message,
-        @lengthNumbers >= 1 ? true : min_value_numbers_message,
-        @lengthNumbers <= 30 ? true : max_value_numbers_message,
-        !!@setNumbers == @setNumbers ? true : set_numbers_should_boolean_message,
-        !!@leftNumbers == @leftNumbers ? true : left_numbers_should_boolean_message,
-        !!@rightNumbers == @rightNumbers ? true : right_numbers_should_boolean_message
+        @length_letters == @length_letters.to_i ? true : should_integer_letters_message,
+        @length_letters <= 50 ? true : max_value_letters_message,
+        @length_letters >= 0 ? true : min_value_letters_messages,
+        @length_numbers == @length_numbers.to_i ? true : should_integer_numbers_message,
+        @length_numbers >= 1 ? true : min_value_numbers_message,
+        @length_numbers <= 30 ? true : max_value_numbers_message,
+        !!@set_numbers == @set_numbers ? true : set_numbers_should_boolean_message,
+        !!@left_numbers == @left_numbers ? true : left_numbers_should_boolean_message,
+        !!@right_numbers == @right_numbers ? true : right_numbers_should_boolean_message
       ]
 
 
@@ -25,6 +29,5 @@ module Validation
     return false if valid_mass.include?(false)
 
     true
-
   end
 end
